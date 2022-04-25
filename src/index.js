a38439d38f8d0298932eaa0cecf0b230a7a61387
Carlos Quispe <carlosql2598@gmail.com>
2022-04-25 17:48:27 -0500

    Añadidos archivos y librerías necesarias

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
 
-app.set('port', 3000)
+// git push heroku main = Para subir los cambios a heroku.
 
-app.get('/', (req, res) => {
-    res.send("Hola a todos")
-})
+app.use(bodyParser.json());
+app.set('json spaces', 2);
 
-app.listen(app.get('port'), () => {
-    console.log("Aplicación corriendo en el puerto 3000")
-})
+
+app.listen(PORT, ()=> console.log(`Servidor ejecutandose en el puerto ${PORT}`));
+
+
+//Configuraciones
+app.use(cors());
+
+
+//routers
+app.use('/api/', require('./routers/index'));
+app.use('/api/commentary',require('./routers/commentary'));
+// app.use('/api/usuario', require('./routers/usuario'));
+// app.use('/api/order', require('./routers/order'));
+// app.use('/api/calificacion', require('./routers/calificacion'));
+// app.use('/api/categoria', require('./routers/categoria'));
