d91ba5e9fc3a1e779e4ef52bf7b5e643167a85c7
Carlos Quispe <carlosql2598@gmail.com>
2022-05-01 23:38:32 -0500

    Terminado CRUD proveedores

--- src/routers/proveedor.js
+++ src/routers/proveedor.js
@@ -3,65 +3,141 @@ const router = Router();
 const mysql = require('../conexionbd');
 const { check,validationResult } = require('express-validator');
 
-router.post('/insert', 
+function send_response(status_code, message, error=null, res) {
+    res.status(status_code).json({"status": status_code, "message": message, "error": error});
+}
+
+router.post('/insertar', 
     [
-        check('COM_COMMENTARY', 'La variable COM_COMMENTARY debe contener entre 1 y 100 caracteres.').isLength({min: 1,max: 100}),
-        check('SIG_ID', 'La variable SIG_ID no es un número.').isEmpty()
+        check('PROV_NOMBRE', 'La variable PROV_NOMBRE debe contener entre 1 y 45 caracteres.').isLength({min: 1, max: 45}),
+        check('PROV_CELULAR', 'La variable PROV_CELULAR debe contener entre 12 caracteres (Ejm: +51987654321).').isLength({min: 12, max: 12}),
+        check('PROV_RUC', 'La variable PROV_RUC debe contener entre 11 caracteres.').isLength({min: 11, max: 11})
     ],
     (req, res) => {
     
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
     
-    const query = 'INSERT INTO COMMENTS(COM_COMMENTARY, SIG_ID) VALUES (?, ?);';
+    const query = 'INSERT INTO proveedores (prov_nombre, prov_celular, prov_ruc) VALUES (?, ?, ?);';
     
-    const {COM_COMMENTARY, SIG_ID} = req.body;
+    const { PROV_NOMBRE, PROV_CELULAR, PROV_RUC } = req.body;
 
-    mysql.query(query, [COM_COMMENTARY, SIG_ID], (err, rows) => {
-        if(!err){
-            res.status(201).json({"status": 201, "message": "Solicitud ejecutada exitosamente."});
-        }else{
-            res.status(500).json({"message": "Hubo un error en la consulta en la BD.", "status": 500, "error": err});
+    mysql.query(query, [PROV_NOMBRE, PROV_CELULAR, PROV_RUC], (err, rows) => {
+        if(!err) {
+            res.status(201).json({"status": 201, "message": "Solicitud ejecutada exitosamente.", "error": err});
+        } 
+        else {
+            res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la base de datos.", "error": err});
         }
     } );
-
-    
-
 });
 
 
-//LISTAR TODOS LOS COMENTARIOS CON RESPECTO A UN PRODUCTO.
+router.put('/actualizar', 
+    [
+        check('PROV_ID', 'La variable PROV_ID debe ser un entero positivo.').isInt(),
+        check('PROV_NOMBRE', 'La variable PROV_NOMBRE debe contener entre 1 y 45 caracteres.').isLength({min: 1, max: 45}),
+        check('PROV_CELULAR', 'La variable PROV_CELULAR debe contener entre 12 caracteres (Ejm: +51987654321).').isLength({min: 12, max: 12}),
+        check('PROV_RUC', 'La variable PROV_RUC debe contener entre 11 caracteres.').isLength({min: 11, max: 11})
+    ],
+    (req, res) => {
+    
+    const errors = validationResult(req);
 
-router.get('/getRandom', 
-    // [
-    //     check('id', 'La variable id no es un número').notEmpty().isInt()
-    // ],
-    (req, res)=> {
-        
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-    
-        // const {id} = req.params;
-    
-        const query = 'SELECT COM_COMMENTARY FROM COMMENTS ORDER BY RAND() LIMIT 1';
-    
-    
-        mysql.query(query, (err, rows) => {
-            if(!err){
-                res.status(200).json(rows);
-            }else{
-                res.status(500).json({"message": "Hubo un error en la consulta en la BD.", "status": 500, "error": err});
-            }
-        } );
-
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
-);
+    const queryUpdate = 'UPDATE proveedores SET prov_nombre = ?, prov_celular = ?, prov_ruc = ? WHERE prov_id = ?;';
+    const queryGetId = 'SELECT prov_id FROM proveedores WHERE prov_id = ?';
+    const { PROV_ID, PROV_NOMBRE, PROV_CELULAR, PROV_RUC } = req.body;
+    
+    mysql.query(queryGetId, [PROV_ID], (err, rows) => {
+        if(!err) {
+            if (rows.length === 0) {
+                res.status(404).json({"status": 404, "message": "El proveedor no se encuentra registrado en la base de datos.", "error": err});
+            }
+            else {
+                mysql.query(queryUpdate, [PROV_NOMBRE, PROV_CELULAR, PROV_RUC, PROV_ID], (err, rows) => {
+                    if(!err) {
+                        res.status(201).json({"status": 201, "message": "Solicitud ejecutada exitosamente.", "error": err});
+                    }
+                    else {
+                        res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la BD.", "error": err});
+                    }
+                });
+            }
+        } 
+        else {
+            res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la base de datos.", "error": err});
+        }
+    });
+});
+
+
+router.delete('/eliminar/:PROV_ID', 
+    [
+        check('PROV_ID', 'La variable PROV_ID debe ser un entero positivo.').isInt()
+    ],
+    (req, res) => {
+    
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const queryDelete = 'UPDATE proveedores SET prov_estado = 0 WHERE prov_id = ?;'
+    const queryGetId = 'SELECT prov_id FROM proveedores WHERE prov_id = ?';
+    const { PROV_ID } = req.params;
+    
+    mysql.query(queryGetId, [PROV_ID], (err, rows) => {
+        if(!err) {
+            if (rows.length === 0) {
+                res.status(404).json({"status": 404, "message": "El proveedor no se encuentra registrado en la base de datos.", "error": err});
+            }
+            else {
+                mysql.query(queryDelete, [PROV_ID], (err, rows) => {
+                    if(!err) {
+                        res.status(200).json({"status": 200, "message": "Solicitud ejecutada exitosamente.", "error": err});
+                    }
+                    else {
+                        res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la BD.", "error": err});
+                    }
+                });
+            }
+        }
+        else {
+            res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la base de datos.", "error": err});
+        }
+    });
+});
+
+
+//LISTAR Y BUSCAR LOS PROVEEDORES.
+
+router.get('/buscar/:BUSQ_PARAM?',
+    (req, res) => {
+
+    const query = 'SELECT prov_id, prov_nombre, prov_celular, prov_ruc \
+                    FROM proveedores \
+                    WHERE (prov_nombre like CONCAT("%", ?, "%") OR prov_celular like CONCAT("%", ?, "%") OR prov_ruc like CONCAT("%", ?, "%")) AND prov_estado = 1;';
+
+    const BUSQ_PARAM = req.params['BUSQ_PARAM'] != undefined ? req.params['BUSQ_PARAM'] : "";
+
+    mysql.query(query, [BUSQ_PARAM, BUSQ_PARAM, BUSQ_PARAM], (err, rows) => {
+        if(!err) {
+            res.status(200).json({"status": 200, "message": "Solicitud ejecutada exitosamente.", "error": err, result: rows});
+        }
+        else {
+            res.status(500).json({"status": 500, "message": "Hubo un error en la consulta en la base de datos.", "error": err});
+        }
+    });
+});
 
 
 
--- src/scriptbd/sentencias.sql
+++ src/scriptbd/sentencias.sql
@@ -1,2 +1,171 @@
-Aquí va el script de la bd
+CREATE DATABASE IF NOT EXISTS `lahormiguitadb`;
+
+USE `lahormiguitadb`;
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE IF NOT EXISTS Usuarios (
+    usu_id int AUTO_INCREMENT,
+    usu_fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usu_nombre varchar(45) NOT NULL,
+    usu_apellidos varchar(45) NOT NULL,
+    usu_dni char(8) UNIQUE NOT NULL,
+    usu_username varchar(45) NOT NULL,
+    usu_contrasena varchar(85) NOT NULL,
+    PRIMARY KEY (usu_id)
+);
+
+CREATE TABLE IF NOT EXISTS Pedidos (
+    ped_id int AUTO_INCREMENT,
+    ped_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ped_estado int DEFAULT 1 NOT NULL,
+    usu_id int NOT NULL,
+    PRIMARY KEY (ped_id),
+    FOREIGN KEY (usu_id) REFERENCES Usuarios(usu_id)
+);
+
+CREATE TABLE IF NOT EXISTS Estados (
+    est_id int AUTO_INCREMENT,
+    est_nombre varchar(45),
+    PRIMARY KEY (est_id)
+);
+
+CREATE TABLE IF NOT EXISTS Etiquetas (
+    eti_id int AUTO_INCREMENT,
+    eti_code varchar(16) UNIQUE NOT NULL,
+    eti_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (eti_id)
+);
+
+CREATE TABLE IF NOT EXISTS Productos (
+    prod_id int AUTO_INCREMENT,
+    prod_nombre varchar(45) NOT NULL,
+    prod_precio float NOT NULL,
+    prod_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    eti_id int NOT NULL,
+    est_id int NOT NULL,
+    PRIMARY KEY (prod_id),
+    FOREIGN KEY (eti_id) REFERENCES Etiquetas(eti_id),
+    FOREIGN KEY (est_id) REFERENCES Estados(est_id)
+);
+
+CREATE TABLE IF NOT EXISTS Pedidos_Productos (
+    ped_prod_id int AUTO_INCREMENT,
+    ped_prod_precio float NOT NULL,
+    ped_prod_cantidad int NOT NULL,
+    prod_id int NOT NULL,
+    ped_id int NOT NULL,
+    PRIMARY KEY (ped_prod_id),
+    FOREIGN KEY (prod_id) REFERENCES Productos(prod_id),
+    FOREIGN KEY (ped_id) REFERENCES Pedidos(ped_id)
+);
+
+CREATE TABLE IF NOT EXISTS Reportes (
+    rep_id int AUTO_INCREMENT,
+    rep_titulo varchar(45) NOT NULL,
+    rep_descripcion text NOT NULL,
+    rep_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usu_id int NOT NULL,
+    PRIMARY KEY (rep_id),
+    FOREIGN KEY (usu_id) REFERENCES Usuarios(usu_id)
+);
+
+CREATE TABLE IF NOT EXISTS Proveedores (
+    prov_id int AUTO_INCREMENT,
+    prov_nombre varchar(45) NOT NULL,
+    prov_celular varchar(12) NOT NULL,
+    prov_ruc varchar(16) UNIQUE NOT NULL,
+    prov_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    prov_estado int DEFAULT 1,
+    PRIMARY KEY (prov_id)
+);
+
+CREATE TABLE IF NOT EXISTS Cotizaciones (
+    cot_id int AUTO_INCREMENT,
+    cot_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cot_cantidad int NOT NULL,
+    usu_id int NOT NULL,
+    prod_id int NOT NULL,
+    PRIMARY KEY (cot_id),
+    FOREIGN KEY (usu_id) REFERENCES Usuarios(usu_id),
+    FOREIGN KEY (prod_id) REFERENCES Productos(prod_id)
+);
+
+CREATE TABLE IF NOT EXISTS Cotizaciones_Proveedores (
+    cot_prod_id int AUTO_INCREMENT,
+    cot_id int NOT NULL,
+    prov_id int NOT NULL,
+    PRIMARY KEY (cot_prod_id),
+    FOREIGN KEY (cot_id) REFERENCES Cotizaciones(cot_id),
+    FOREIGN KEY (prov_id) REFERENCES Proveedores(prov_id)
+);
+
+CREATE TABLE IF NOT EXISTS Almacenes (
+    alm_id int AUTO_INCREMENT,
+    alm_nombre varchar(45) NOT NULL,
+    alm_direccion varchar(35) NOT NULL,
+    PRIMARY KEY (alm_id)
+);
+
+CREATE TABLE IF NOT EXISTS Almacenes_Productos (
+    alm_prod_id int AUTO_INCREMENT,
+    alm_prod_stock int DEFAULT 0 NOT NULL,
+    alm_id int NOT NULL,
+    prod_id int NOT NULL,
+    PRIMARY KEY (alm_prod_id),
+    FOREIGN KEY (alm_id) REFERENCES Almacenes(alm_id),
+    FOREIGN KEY (prod_id) REFERENCES Productos(prod_id)
+);
+
+CREATE TABLE IF NOT EXISTS Temperaturas (
+    temp_id int AUTO_INCREMENT,
+    temp_valor float NOT NULL,
+    temp_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    alm_id int NOT NULL,
+    PRIMARY KEY (temp_id),
+    FOREIGN KEY (alm_id) REFERENCES Almacenes(alm_id)
+);
+
+CREATE TABLE IF NOT EXISTS Humedades (
+    hum_id int AUTO_INCREMENT,
+    hum_valor float NOT NULL,
+    hum_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    alm_id int NOT NULL,
+    PRIMARY KEY (hum_id),
+    FOREIGN KEY (alm_id) REFERENCES Almacenes(alm_id)
+);
+
+CREATE TABLE IF NOT EXISTS Reportes_Productos (
+    rep_prod_id int AUTO_INCREMENT,
+    rep_id int NOT NULL,
+    prod_id int NOT NULL,
+    PRIMARY KEY (rep_prod_id),
+    FOREIGN KEY (rep_id) REFERENCES Reportes(rep_id),
+    FOREIGN KEY (prod_id) REFERENCES Productos(prod_id)
+);
+
+CREATE TABLE IF NOT EXISTS Categorias (
+    cat_id int AUTO_INCREMENT,
+    cat_nombre varchar(45) NOT NULL,
+    cat_fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (cat_id)
+);
+
+CREATE TABLE IF NOT EXISTS Proveedores_Categorias (
+    prov_cat_id int AUTO_INCREMENT,
+    prov_id int NOT NULL,
+    cat_id int NOT NULL,
+    PRIMARY KEY (prov_cat_id),
+    FOREIGN KEY (prov_id) REFERENCES Proveedores(prov_id),
+    FOREIGN KEY (cat_id) REFERENCES Categorias(cat_id)
+);
+
+CREATE TABLE IF NOT EXISTS Productos_Categorias (
+    prod_cat_id int AUTO_INCREMENT,
+    prod_id int NOT NULL,
+    cat_id int NOT NULL,
+    PRIMARY KEY (prod_cat_id),
+    FOREIGN KEY (prod_id) REFERENCES Productos(prod_id),
+    FOREIGN KEY (cat_id) REFERENCES Categorias(cat_id)
+);
 
